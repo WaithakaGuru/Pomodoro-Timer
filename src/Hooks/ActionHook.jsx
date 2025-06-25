e9ebefa67511d1ccee5d80ccea6af65d8035fb3a
WaithakaGuru <waithakaoffices@gmail.com>
2025-06-26 01:11:43 +0300

    Format entire code base with Prettier Formatter

--- src/Hooks/ActionHook.jsx
+++ src/Hooks/ActionHook.jsx
@@ -2,7 +2,8 @@ import { useContext } from "react";
 import ActionContext from "../Contexts/ActionsContext";
 
 export default function useActions() {
-    const actionsCtx = useContext(ActionContext);
-    if(!actionsCtx) throw new Error("useActions must be used within the ActionContext");
-    return actionsCtx;
+  const actionsCtx = useContext(ActionContext);
+  if (!actionsCtx)
+    throw new Error("useActions must be used within the ActionContext");
+  return actionsCtx;
 }
