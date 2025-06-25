01de6d073fcf69723be25d19a5d92f62868dfcae
WaithakaGuru <waithakaoffices@gmail.com>
2025-06-25 22:50:23 +0300

    Fix the glitch error

--- src/Contexts/ActionsContext.jsx
+++ src/Contexts/ActionsContext.jsx
@@ -18,7 +18,7 @@ export function ActionCtxProvider({children}){
     useEffect(()=>{
         setMode("Start a Focus Session");
     }, []);
-
+    
     function addActiveButton(e){
         setActiveButtonId(e.target.id);
     }
@@ -26,9 +26,9 @@ export function ActionCtxProvider({children}){
     function handleAddTime (e) {
         setTime({mins:e.target.value, secs: 0});
     }
-
+    
     function handleStartFocusSession(e) {
-       addActiveButton(e);
+        addActiveButton(e);
         /**
          * PAUSE MODES 
          * 0 - Start - the count down not yet started, new Focus session e.g on page refresh
@@ -37,18 +37,19 @@ export function ActionCtxProvider({children}){
          * 2 - resume - user has paused the count down and
          *      btn displays 'resume' for user to resume the timer count down 
         */
-        
-        if(isPaused === 0){
-            setIsPaused(1)
+       
+       if(isPaused === 0){
+           setIsPaused(1)
         }else if(isPaused === 1) {
             setMode("Focus session on Hold");
             setIsPaused(2);
         }else{
             setIsPaused(1)
         }
-
+        
         // THE COUNT DOWN LOGIC
-        const totalSessionTime = fullSessionTime(time.mins);
+        const minutes = time.mins
+        const totalSessionTime = fullSessionTime(minutes);
         // let focusSessionInterval;
         if(time.mins > 0 || time.secs > 0){
             setInterval(()=>setTime(calculateTimeRemaining(totalSessionTime)), 980)
